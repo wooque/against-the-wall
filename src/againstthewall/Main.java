de5d2a3b0f5c205c1d63dde898281e2eecedb4d3
Vuk Mirovic <wooque@gmail.com>
2014-04-05 19:53:03 +0200

    Added pad

--- src/againstthewall/Main.java
+++ src/againstthewall/Main.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
@@ -12,13 +14,14 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 @SuppressWarnings("serial")
-public class Main extends JPanel implements Runnable {
+public class Main extends JPanel implements Runnable, MouseMotionListener {
     
     boolean init;
     Point2D.Double stars[];
     Rectangle2D.Double bricks[];
     Color bricksColors[];
     Dimension dim;
+    Rectangle2D.Double pad;
     
     public void init() {
         if (!init) {
@@ -28,14 +31,14 @@ public class Main extends JPanel implements Runnable {
                 stars[i] = new Point2D.Double(Math.random() * dim.width, Math.random() * dim.height);
             }
 
-        	double wallLine = dim.height * 0.1;
-        	double brickWidth = dim.width / 10;
-        	double brickHeigth = dim.height * 0.1;
+            double wallLine = dim.height * 0.1;
+            double brickWidth = dim.width / 10;
+            double brickHeigth = dim.height * 0.1;
             
             bricks = new Rectangle2D.Double[40];
             bricksColors = new Color[40];
             for (int i = 0; i < 4; i++) {
-            	
+                
                 for (int j = 0; j < 10; j++) {
                     bricks[i * 10 + j] = new Rectangle2D.Double(j * (dim.width / 10), wallLine, brickWidth, brickHeigth);
                     bricksColors[i * 10 + j] = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
@@ -43,6 +46,9 @@ public class Main extends JPanel implements Runnable {
                 
                 wallLine += dim.height * 0.1;
             }
+            
+            pad = new Rectangle2D.Double(dim.width * 0.45, (39.0 / 40.0) * dim.height, dim.width / 10, dim.height / 40);
+            
             init = true;
         }
     }
@@ -59,9 +65,15 @@ public class Main extends JPanel implements Runnable {
             for (int i = 0; i < 100; i++) {
                 g2d.fillRect((int)stars[i].x, (int)stars[i].y, 1, 1);
             }
+            
             for (int i = 0; i < 40; i++) {
                 g2d.setPaint(bricksColors[i]);
-            	g2d.fill(bricks[i]);
+                g2d.fill(bricks[i]);
+            }
+            
+            g2d.setColor(Color.WHITE);
+            synchronized (pad) {
+                g2d.fill(pad);
             }
         }
     }
@@ -80,6 +92,18 @@ public class Main extends JPanel implements Runnable {
             }
         }
     }
+    
+    @Override
+    public void mouseDragged(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent event) {
+        synchronized (pad) {
+            if (event.getX() >= 0 && event.getX() <= getSize().width * 0.9)
+                pad.x = event.getX();
+        }
+    }
 
     public static void createAndShowGUI() {
         
@@ -88,6 +112,7 @@ public class Main extends JPanel implements Runnable {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         Main panel = new Main();
+        panel.addMouseMotionListener(panel);
         panel.setOpaque(true);
         frame.add(panel);
         
